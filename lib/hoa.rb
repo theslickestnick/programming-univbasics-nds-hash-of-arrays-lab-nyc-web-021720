4391d696177602e308e703f1fe03c7f624ee7327
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-27 21:00:39 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,6 +6,6 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  # Write your implementation here
+  show[name] << "Dave"# Write your implementation here
   # Should return the array of the 'show' argument
 end
